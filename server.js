6d9fbb7d79adcc3cfe9c5d9adf08a9a086b413dc
Ludvig Nyqvist <ludvig.nyqvist1@outlook.com>
2021-09-29 15:05:57 +0200

    Used fs module to write index.html

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
-const http = require('http'); //Initializes the http pack from nodeJs
+const http = require('http'); //Imports modules froms nodeJs
+const fs = requrie('fs');   //File system module, read write and read.
+
 
 const server = http.createServer((req, res) =>{
-    res.setHeader('Content-type', 'text/html');
-    res.end('Hello world, server is up and running!')
+    fs.readFile('index.html', (err, data)=>{
+        res.writeHead('200', {'Content-type' : 'text/html'});
+        res.write(data);
+        res.end();
+
+    });
 });
 
-console.log('Sever is up and running on this current computer');
+console.log('Sever is up and running on this computer!');
 server.listen(8089);
